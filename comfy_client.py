90d8eca296d06b83e3d563748382141a178b8278
WillReynolds5 <willmcleanreynolds@gmail.com>
2024-02-11 11:27:04 -0500

    added comfyui api generator

---
+++ comfy_client.py
@@ -0,0 +1,131 @@
+import random
+import json
+from urllib import request
+
+def comfyui_inference(positive_prompt, negative_prompt):
+
+
+    random_seed = random.randint(0, 1000000000)
+    prompt = """
+{{
+  "3": {{
+    "inputs": {{
+      "seed": {random_seed},
+      "steps": 20,
+      "cfg": 8,
+      "sampler_name": "euler",
+      "scheduler": "normal",
+      "denoise": 1,
+      "model": [
+        "4",
+        0
+      ],
+      "positive": [
+        "6",
+        0
+      ],
+      "negative": [
+        "7",
+        0
+      ],
+      "latent_image": [
+        "5",
+        0
+      ]
+    }},
+    "class_type": "KSampler",
+    "_meta": {{
+      "title": "KSampler"
+    }}
+  }},
+  "4": {{
+    "inputs": {{
+      "ckpt_name": "BEN_USE_THIS/sd_xl_base_1.0.safetensors"
+    }},
+    "class_type": "CheckpointLoaderSimple",
+    "_meta": {{
+      "title": "Load Checkpoint"
+    }}
+  }},
+  "5": {{
+    "inputs": {{
+      "width": 1024,
+      "height": 1024,
+      "batch_size": 1
+    }},
+    "class_type": "EmptyLatentImage",
+    "_meta": {{
+      "title": "Empty Latent Image"
+    }}
+  }},
+  "6": {{
+    "inputs": {{
+      "text": "{positive}",
+      "clip": [
+        "4",
+        1
+      ]
+    }},
+    "class_type": "CLIPTextEncode",
+    "_meta": {{
+      "title": "CLIP Text Encode (Prompt)"
+    }}
+  }},
+  "7": {{
+    "inputs": {{
+      "text": "{negative}",
+      "clip": [
+        "4",
+        1
+      ]
+    }},
+    "class_type": "CLIPTextEncode",
+    "_meta": {{
+      "title": "CLIP Text Encode (Prompt)"
+    }}
+  }},
+  "8": {{
+    "inputs": {{
+      "samples": [
+        "3",
+        0
+      ],
+      "vae": [
+        "4",
+        2
+      ]
+    }},
+    "class_type": "VAEDecode",
+    "_meta": {{
+      "title": "VAE Decode"
+    }}
+  }},
+  "9": {{
+    "inputs": {{
+      "filename_prefix": "ComfyUI",
+      "images": [
+        "8",
+        0
+      ]
+    }},
+    "class_type": "SaveImage",
+    "_meta": {{
+      "title": "Save Image"
+    }}
+  }}
+}}""".format(positive=positive_prompt, negative=negative_prompt, random_seed=random_seed)
+    json_prompt = json.loads(prompt)
+    return queue_prompt(json_prompt)
+
+
+def queue_prompt(prompt):
+    p = {"prompt": prompt}
+    data = json.dumps(p).encode('utf-8')
+    req = request.Request("http://127.0.0.1:8188/prompt", data=data)
+    return request.urlopen(req)
+
+
+if __name__ == "__main__":
+    positive = "white women"
+    negative = "deformed body and face"
+    print(comfyui_inference(positive, negative))
---
+++ gen_dataset.py
@@ -0,0 +1,78 @@
+import json
+import time
+import openai
+
+from comfy_client import comfyui_inference
+
+openai_client = openai.Client(
+    api_key="fd4c8cd310d4bdfa853e55f3d4233438d991e4cdfe26db702b88a3a5b1f7253b",
+    base_url="https://api.together.xyz/v1",
+)
+
+previous_prompts = []
+
+
+def parse_json(json_string):
+    try:
+        return json.loads(json_string), True
+    except json.JSONDecodeError:
+        return json_string, False
+
+
+def inference(messages, model_kwargs={"temperature": 1}):
+    response = openai_client.chat.completions.create(
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+        messages=messages,
+        max_tokens=256,
+        **model_kwargs,
+    )
+    return response.choices[0].message.content
+
+
+def main():
+    global previous_prompts
+
+    for i in range(10000):
+        try:
+            base_prompt = """
+You are a “prompt” generating assistant. Your job is to generate image prompts for AI generated images of human characters faces. Each image will be a head shot.
+Prompts are lists of phrases, separated by commas. Each phrase in a prompt describes a different aspect of the character / scene being generated. 
+Parentheses can be used to give extra strength to a phrase in a prompt. 
+Each prompt has a “positive” and “negative”. The generated images will correlate with the positive prompt and not correlate with the negative. 
+Here is an example prompt:
+
+{
+    “positive”: “head shot, (hyper realistic animation), blonde city girl,  seductive expression”,
+    “negative”: “deformed face, disfigured eyes”
+}
+
+Generate a new prompt that is very different from the following previous prompts:"""
+
+            # Dynamically add the previous prompts to the base prompt for each iteration
+            if previous_prompts:
+                for prompt in previous_prompts:
+                    base_prompt += f"\n\n{prompt}"
+
+            base_prompt += "\nThe new prompt should describe and vary in style, gender, ethnicity, age, pose, expression. Output only one JSON object wth the keys postive and negative."
+
+            messages = [
+                {"role": "user", "content": base_prompt}
+            ]
+
+            response = inference(messages)
+            parsed_response, success = parse_json(response)
+            if success:
+                print(response)
+                # Update the previous prompts list with the new prompt
+                previous_prompts.append(response)
+                # Ensure we only keep the last two prompts
+                previous_prompts = previous_prompts[-10:]
+                comfyui_inference(positive_prompt=parsed_response["positive"], negative_prompt=parsed_response["negative"])
+                time.sleep(0.5)
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
