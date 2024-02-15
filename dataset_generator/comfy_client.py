7d9cd980c6d3736bb3017bd24cc9398ecfd288b8
WillReynolds5 <willmcleanreynolds@gmail.com>
2024-02-15 23:19:56 +0100

    update

--- dataset_generator/comfy_client.py
+++ dataset_generator/comfy_client.py
@@ -4,7 +4,6 @@ from urllib import request
 
 def comfyui_inference_basic(positive_prompt, negative_prompt, id):
 
-
     random_seed = random.randint(0, 1000000000)
     prompt = """
 {{
@@ -102,7 +101,7 @@ def comfyui_inference_basic(positive_prompt, negative_prompt, id):
   }},
   "9": {{
     "inputs": {{
-      "filename_prefix": {id},
+      "filename_prefix": "{id}",
       "images": [
         "8",
         0
@@ -119,7 +118,264 @@ def comfyui_inference_basic(positive_prompt, negative_prompt, id):
 
 
 def comfyui_inference_ipadpater():
-    pass
+
+    prompt = """
+{
+  "1": {
+    "inputs": {
+      "ckpt_name": "RealVisXL_V3.0.safetensors"
+    },
+    "class_type": "CheckpointLoaderSimple",
+    "_meta": {
+      "title": "Load Checkpoint"
+    }
+  },
+  "3": {
+    "inputs": {
+      "ipadapter_file": "ip-adapter-plus-face_sdxl_vit-h.safetensors"
+    },
+    "class_type": "IPAdapterModelLoader",
+    "_meta": {
+      "title": "Load IPAdapter Model"
+    }
+  },
+  "4": {
+    "inputs": {
+      "clip_name": "sd1.5/BEN_USE_THIS/model.safetensors"
+    },
+    "class_type": "CLIPVisionLoader",
+    "_meta": {
+      "title": "Load CLIP Vision"
+    }
+  },
+  "5": {
+    "inputs": {
+      "weight": 0.8,
+      "noise": 0,
+      "weight_type": "original",
+      "start_at": 0,
+      "end_at": 1,
+      "unfold_batch": false,
+      "ipadapter": [
+        "3",
+        0
+      ],
+      "clip_vision": [
+        "4",
+        0
+      ],
+      "image": [
+        "6",
+        0
+      ],
+      "model": [
+        "1",
+        0
+      ]
+    },
+    "class_type": "IPAdapterApply",
+    "_meta": {
+      "title": "Apply IPAdapter"
+    }
+  },
+  "6": {
+    "inputs": {
+      "image": "0c33596bc55c4f4ea4e973ab4f8071a3_00001_.png",
+      "upload": "image"
+    },
+    "class_type": "LoadImage",
+    "_meta": {
+      "title": "Load Image"
+    }
+  },
+  "7": {
+    "inputs": {
+      "text": "standing in the kitchen with back on a fridge",
+      "clip": [
+        "1",
+        1
+      ]
+    },
+    "class_type": "CLIPTextEncode",
+    "_meta": {
+      "title": "CLIP Text Encode (Prompt)"
+    }
+  },
+  "8": {
+    "inputs": {
+      "text": "",
+      "clip": [
+        "1",
+        1
+      ]
+    },
+    "class_type": "CLIPTextEncode",
+    "_meta": {
+      "title": "CLIP Text Encode (Prompt)"
+    }
+  },
+  "9": {
+    "inputs": {
+      "seed": [
+        "17",
+        0
+      ],
+      "steps": 25,
+      "cfg": 6,
+      "sampler_name": "dpmpp_2m_sde_gpu",
+      "scheduler": "karras",
+      "denoise": 1,
+      "model": [
+        "5",
+        0
+      ],
+      "positive": [
+        "7",
+        0
+      ],
+      "negative": [
+        "8",
+        0
+      ],
+      "latent_image": [
+        "10",
+        0
+      ]
+    },
+    "class_type": "KSampler",
+    "_meta": {
+      "title": "KSampler"
+    }
+  },
+  "10": {
+    "inputs": {
+      "width": 1024,
+      "height": 1024,
+      "batch_size": 1
+    },
+    "class_type": "EmptyLatentImage",
+    "_meta": {
+      "title": "Empty Latent Image"
+    }
+  },
+  "11": {
+    "inputs": {
+      "samples": [
+        "9",
+        0
+      ],
+      "vae": [
+        "1",
+        2
+      ]
+    },
+    "class_type": "VAEDecode",
+    "_meta": {
+      "title": "VAE Decode"
+    }
+  },
+  "15": {
+    "inputs": {
+      "images": [
+        "11",
+        0
+      ]
+    },
+    "class_type": "PreviewImage",
+    "_meta": {
+      "title": "Preview Image"
+    }
+  },
+  "17": {
+    "inputs": {
+      "seed": -1
+    },
+    "class_type": "Seed (rgthree)",
+    "_meta": {
+      "title": "Seed (rgthree)"
+    }
+  },
+  "18": {
+    "inputs": {
+      "enabled": true,
+      "swap_model": "inswapper_128.onnx",
+      "facedetection": "retinaface_resnet50",
+      "face_restore_model": "codeformer.pth",
+      "face_restore_visibility": 1,
+      "codeformer_weight": 1,
+      "detect_gender_source": "no",
+      "detect_gender_input": "no",
+      "source_faces_index": "0",
+      "input_faces_index": "0",
+      "console_log_level": 1,
+      "source_image": [
+        "6",
+        0
+      ],
+      "input_image": [
+        "11",
+        0
+      ]
+    },
+    "class_type": "ReActorFaceSwap",
+    "_meta": {
+      "title": "ReActor - Fast Face Swap"
+    }
+  },
+  "21": {
+    "inputs": {
+      "images": [
+        "18",
+        0
+      ]
+    },
+    "class_type": "PreviewImage",
+    "_meta": {
+      "title": "Preview Image"
+    }
+  },
+  "22": {
+    "inputs": {
+      "facedetection": "retinaface_resnet50",
+      "codeformer_fidelity": 1,
+      "facerestore_model": [
+        "23",
+        0
+      ],
+      "image": [
+        "18",
+        0
+      ]
+    },
+    "class_type": "FaceRestoreCFWithModel",
+    "_meta": {
+      "title": "FaceRestoreCFWithModel"
+    }
+  },
+  "23": {
+    "inputs": {
+      "model_name": "codeformer.pth"
+    },
+    "class_type": "FaceRestoreModelLoader",
+    "_meta": {
+      "title": "FaceRestoreModelLoader"
+    }
+  },
+  "24": {
+    "inputs": {
+      "filename_prefix": "2024-02-11/faceswapped",
+      "images": [
+        "22",
+        0
+      ]
+    },
+    "class_type": "SaveImage",
+    "_meta": {
+      "title": "Save Image"
+    }
+  }
+}
+"""
 
 def queue_prompt(prompt):
     p = {"prompt": prompt}
---
+++ dataset_generator/gen_repose.py
@@ -0,0 +1,91 @@
+import json
+import uuid
+import time
+import openai
+
+from comfy_client import comfyui_inference_basic
+from llm import parse_json, inference
+
+previous_prompts = []
+
+
+def save_prompt_to_json(filename, prompt_data):
+    """Save prompt data to a JSON file."""
+    try:
+        # Load existing data
+        with open('prompts_data.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        # If the file does not exist, start with an empty list
+        data = []
+
+    # Append the new data
+    data.append(prompt_data)
+
+    # Write the updated data back to the file
+    with open('prompts_data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def generate_portraits():
+    global previous_prompts
+
+    for _ in range(10000):
+        try:
+            base_prompt = """
+You are a “prompt” generating assistant. Your job is to generate image prompts for AI generated images of human characters. Each image will be a different shot of one or multiple people doing human things.
+Prompts are lists of phrases, separated by commas. Each phrase in a prompt describes a different aspect of the character / scene being generated. 
+Parentheses can be used to give extra strength to a phrase in a prompt. 
+Each prompt has a “positive” and “negative”. The generated images will correlate with the positive prompt and not correlate with the negative. 
+Here is an example prompt:
+
+{
+    “positive”: “(hyper realistic animation), blonde city girl,  (walking down the street in santa monica wearing bikini top), seductive expression”,
+    “negative”: “deformed face, disfigured eyes, bad weather”
+}
+
+Generate a new prompt that is very different from the following previous prompts:"""
+
+            # Dynamically add the previous prompts to the base prompt for each iteration
+            if previous_prompts:
+                for prompt in previous_prompts:
+                    base_prompt += f"\n\n{prompt}"
+
+            base_prompt += "\nThe new prompt should describe and vary in style, character, pose, expression and activity. Output only one JSON object with the keys positive and negative."
+
+            messages = [
+                {"role": "user", "content": base_prompt}
+            ]
+
+            response = inference(messages)
+            parsed_response, success = parse_json(response)
+            prefix = str(uuid.uuid4().hex)
+            if success:
+                print(response)
+                # Update the previous prompts list with the new prompt
+                previous_prompts.append(response)
+                # Ensure we only keep the last two prompts
+                previous_prompts = previous_prompts[-10:]
+                comfyui_inference_basic(positive_prompt=parsed_response["positive"],
+                                        negative_prompt=parsed_response["negative"],
+                                        id=prefix)
+
+                # TODO: get the filename
+                # Save the prompt along with the filename and prompt details to a JSON file
+                prompt_data = {
+                    "id": prefix,
+                    "prompt": {
+                        "positive": parsed_response["positive"],
+                        "negative": parsed_response["negative"]
+                    }
+                }
+                save_prompt_to_json('prompts_data.json', prompt_data)
+
+                time.sleep(0.5)
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    generate_portraits()
