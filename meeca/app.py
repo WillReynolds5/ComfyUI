dcc08e40d02805061d3f6b1a279da42962258595
WillReynolds5 <willmcleanreynolds@gmail.com>
2024-09-15 21:20:15 -0600

    using ngrok

--- meeca/app.py
+++ meeca/app.py
@@ -7,7 +7,8 @@ import os
 from PIL import Image
 
 # Set the server address for your API
-server_address = "http://174.52.227.249:8188"
+url = "https://1314-2601-681-8700-b290-7300-d115-8289-9b9c.ngrok-free.app"
+server_address = "http://{url}:8188"
 
 
 def gen_prompt(pos_prompt, neg_prompt, background_prompt, face_image, pose_image, seed=None):
